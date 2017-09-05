ddd613ec8ab9069dcb00d90e162c6180b84a7754
Austin Gibbons <austingibb@gmail.com>
2017-09-05 12:00:22 -0600

    movement

--- otherGame.js
+++ otherGame.js
@@ -1,9 +1,45 @@
+var x, y, ctx;
+
 function init() {
 	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	ctx.moveTo(0,0);
-	ctx.lineTo(100,100);
+	ctx = c.getContext("2d");
+}
+
+function drawCircle(ctx, x, y, r) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2*Math.PI);
 	ctx.stroke();
 }
 
+function draw() {
+	drawCircle(ctx, x, y, 5);
+}
+
+function loop(timestamp) {
+  draw();
+  window.requestAnimationFrame(loop)
+}
+
+window.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 37: // Left
+	  x -= 2;
+	break;
+
+    case 38: // Up
+	  y += 2;
+    break;
+
+    case 39: // Right
+      x += 2;
+    break;
+
+    case 40: // Down
+      y -= 2;
+    break;
+  }
+}, false);
+
 window.onload = init;
+
+window.requestAnimationFrame(loop);
