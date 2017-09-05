789ed76a2ed9fcf6ca6479263e035c220d552ea0
Austin Gibbons <austingibb@gmail.com>
2017-09-05 11:32:58 -0600

    onload

--- otherGame.js
+++ otherGame.js
@@ -1,5 +1,9 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(100,100);
-ctx.stroke();
+function init() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(0,0);
+	ctx.lineTo(100,100);
+	ctx.stroke();
+}
+
+window.onload = init;
